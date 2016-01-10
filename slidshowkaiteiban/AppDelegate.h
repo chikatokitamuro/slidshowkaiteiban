761cf3ea23ac2a153b9e7c1bd5edc7218bf7f567
chikato ゲーム用 kitamuro <despitefoster@yahoo.co.jp>
2016-01-10 20:15:43 +0900

    Initial Commit

---
+++ slidshowkaiteiban/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  slidshowkaiteiban
+//
+//  Created by chikatokitamuro on 2016/01/10.
+//  Copyright © 2016年 chikatokitamuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ slidshowkaiteiban/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  slidshowkaiteiban
+//
+//  Created by chikatokitamuro on 2016/01/10.
+//  Copyright © 2016年 chikatokitamuro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
